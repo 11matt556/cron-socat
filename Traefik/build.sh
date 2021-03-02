bc290e08c37f6638bb2632d12ef2164b1d5e0de9
djpic <djpic@icloud.com>
2021-03-01 19:44:33 -0500

    Remove custom traffic build.

--- Traefik/build.sh
+++ Traefik/build.sh
@@ -16,14 +16,3 @@ docker push djpic/traefik:tls
 # Post Clean Up
 docker rmi djpic/traefik:latest
 docker rmi djpic/traefik:tls
-
-
-#Build Traefik with static configuration
-cd static
-docker build --tag djpic/custom-traefik:latest .
-
-#Push to Repo
-docker push djpic/custom-traefik:latest
-
-#Post Clean Up
-docker rmi djpic/custom-traefik:latest
