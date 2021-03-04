dfcb617ce9647a920c5a09453befb6a852e3b7af
djpic <djpic@icloud.com>
2021-03-03 21:06:51 -0500

    Updated and reorganized script to remove excess commands.

--- Nginx/build.sh
+++ Nginx/build.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
 
+# Clean old images
+docker rmi djpic/nginx:standard djpic/nginx:tls djpic/nginx:phpfpm djpic/nginx:latest djpic/nginx:phpfpm-tls
+
+
 # build standard ngnix image
-docker rmi djpic/nginx:latest
 docker build --tag djpic/nginx:standard .
-docker tag djpic/nginx:standard djpic/nginx:latest
-docker push djpic/nginx
-docker rmi djpic/nginx:latest djpic/nginx:standard
+
+# build phpfpm NGINX image
+cd phpfpm
+docker build --tag djpic/nginx:phpfpm .
+docker tag djpic/nginx:phpfpm djpic/nginx:latest
+
+
+# Create Certificate
+cd ../
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout localhost.key -out localhost.crt -extensions san -config <(echo "[req]"; echo distinguished_name=req; echo "[san]"; echo subjectAltName=DNS:localhost) -subj /CN=localhost
+cp localhost.key tls/localhost.key
+cp localhost.crt tls/localhost.crt
+mv localhost.key phpfpm/tls/localhost.key
+mv localhost.crt phpfpm/tls/localhost.crt
+
 
 # Build tls NGINX image
 cd tls
-openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout localhost.key -out localhost.crt -extensions san -config <(echo "[req]"; echo distinguished_name=req; echo "[san]"; echo subjectAltName=DNS:localhost) -subj /CN=localhost
-docker rmi djpic/nginx:tls
 docker build --tag djpic/nginx:tls .
-docker push djpic/nginx:tls
-docker rmi djpic/nginx:tls
-rm localhost.key
-rm localhost.crt
+rm localhost.crt localhost.key
 
-
-# build phpfpm NGINX image
-cd ../phpfpm/
-docker rmi djpic/nginx:phpfpm
-docker build --tag djpic/nginx:phpfpm .
-docker push djpic/nginx:phpfpm
-docker rmi djpic/nginx:phpfpm
 
 # build phpfpm NGINX image with tls
-cd tls
-openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout localhost.key -out localhost.crt -extensions san -config <(echo "[req]"; echo distinguished_name=req; echo "[san]"; echo subjectAltName=DNS:localhost) -subj /CN=localhost
-docker rmi djpic/nginx:phpfpm-tls
+cd ../phpfpm/tls
 docker build --tag djpic/nginx:phpfpm-tls .
+rm localhost.crt localhost.key
+
+
+# Push Images
+docker push djpic/nginx:standard
+docker push djpic/nginx:tls
+docker push djpic/nginx:phpfpm
+docker push djpic/nginx:latest
 docker push djpic/nginx:phpfpm-tls
-docker rmi djpic/nginx:phpfpm-tls
-rm localhost.key
-rm localhost.crt
+
+# Remove Images
+docker rmi djpic/nginx:standard djpic/nginx:tls djpic/nginx:phpfpm djpic/nginx:latest djpic/nginx:phpfpm-tls
