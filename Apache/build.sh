0d39d2115778f3ff347af37d59976e3bca48d41d
djpic <djpic@icloud.com>
2021-04-20 15:30:21 -0400

    Remove apache image files.  Apache is no longer used.

--- Apache/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# build phpfpm NGINX image
-cd php
-docker rmi djpic/apache:phpfpm
-docker build --tag djpic/apache:phpfpm .
-docker push djpic/apache:phpfpm
-docker rmi djpic/apache:phpfpm
