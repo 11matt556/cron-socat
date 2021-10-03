9f410e2da34912b6b3e7f51b64dd8850ef428e8a
djpic <djpic@icloud.com>
2021-10-03 11:24:35 -0400

    Update script name to use the standard scheduled jobs private folder.

--- Cron/default/script.sh
+++ Cron/default/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Running default command...." > /dev/stdout
-SCRIPT_NAME=/app/private/cron.php \
-SCRIPT_FILENAME=/app/private/cron.php \
+SCRIPT_NAME=/app/private/scheduled_jobs/1min.php \
+SCRIPT_FILENAME=/app/private/scheduled_jobs/1min.php \
 REQUEST_METHOD=GET \
 cgi-fcgi -bind -connect php:9000
