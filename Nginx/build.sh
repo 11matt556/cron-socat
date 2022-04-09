d5480e1d208665fd0e9bcfedafcfc7e6bf6f84db
djpic <djpic@icloud.com>
2022-04-09 15:18:47 -0400

    Add openssl configuration file for nginx tls images.

--- Nginx/build.sh
+++ Nginx/build.sh
@@ -10,7 +10,7 @@ docker tag djpic/nginx:phpfpm djpic/nginx:latest
 
 # Create Certificate
 cd ../
-openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout localhost.key -out localhost.crt -extensions san -config <(echo "[req]"; echo distinguished_name=req; echo "[san]"; echo subjectAltName=DNS:localhost) -subj /CN=localhost
+openssl req -x509 -nodes -days 3650 -newkey rsa:4096 -keyout localhost.key -out localhost.crt -config openssl.conf -extensions 'v3_req'
 cp localhost.key tls/localhost.key
 cp localhost.crt tls/localhost.crt
 mv localhost.key phpfpm/tls/localhost.key
@@ -27,11 +27,11 @@ docker build --tag djpic/nginx:phpfpm-tls .
 rm localhost.crt localhost.key
 
 # Push Images
-docker push djpic/nginx:standard
-docker push djpic/nginx:tls
-docker push djpic/nginx:phpfpm
-docker push djpic/nginx:latest
-docker push djpic/nginx:phpfpm-tls
+#docker push djpic/nginx:standard
+#docker push djpic/nginx:tls
+#docker push djpic/nginx:phpfpm
+#docker push djpic/nginx:latest
+#docker push djpic/nginx:phpfpm-tls
 
 # Create Down for Maintenance Image
 cd ../../downformaintenance
