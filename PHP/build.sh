db4d7e06eb5a2d35526be335a7d324d34737d404
djpic <djpic@icloud.com>
2022-11-28 13:31:43 -0500

    Add additional versions to loop through for PHP.

--- PHP/build.sh
+++ PHP/build.sh
@@ -1,25 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-current_php_version=8.0.25
+# declare an array called array and define 3 vales
+php_versions=(8.0.25 8.1.12)
 
-# Build apache image with mysqli and memcached extension
-cd MySQLi
-docker build --build-arg php_version=$current_php_version --tag djpic/php:$current_php_version-mysqli .
+for php_version in "${php_versions[@]}"
+do
+  # Build apache image with mysqli and memcached extension
+  cd MySQLi
+  docker build --build-arg php_version=$php_version --tag djpic/php:$php_version-mysqli .
 
-# Build image with browscap file
-cd browscap
-wget -O browscap.ini https://browscap.org/stream?q=PHP_BrowsCapINI
-docker build --build-arg php_version=$current_php_version --tag djpic/php:$current_php_version-mysqli-browscap .
-rm browscap.ini
-wget -O browscap.ini https://browscap.org/stream?q=Full_PHP_BrowsCapINI
-docker build --build-arg php_version=$current_php_version --tag djpic/php:$current_php_version-mysqli-full_php_browscap .
-rm browscap.ini
-wget -O browscap.ini https://browscap.org/stream?q=Lite_PHP_BrowsCapINI
-docker build --build-arg php_version=$current_php_version --tag djpic/php:$current_php_version-mysqli-lite_php_browscap .
-rm browscap.ini
+  # Build image with browscap file
+  cd browscap
+  wget -O browscap.ini https://browscap.org/stream?q=PHP_BrowsCapINI
+  docker build --build-arg php_version=$php_version --tag djpic/php:$php_version-mysqli-browscap .
+  rm browscap.ini
+  wget -O browscap.ini https://browscap.org/stream?q=Full_PHP_BrowsCapINI
+  docker build --build-arg php_version=$php_version --tag djpic/php:$php_version-mysqli-full_php_browscap .
+  rm browscap.ini
+  wget -O browscap.ini https://browscap.org/stream?q=Lite_PHP_BrowsCapINI
+  docker build --build-arg php_version=$php_version --tag djpic/php:$php_version-mysqli-lite_php_browscap .
+  rm browscap.ini
 
-# Docker Push Image
-docker push djpic/php:$current_php_version-mysqli
-docker push djpic/php:$current_php_version-mysqli-browscap
-docker push djpic/php:$current_php_version-mysqli-full_php_browscap
-docker push djpic/php:$current_php_version-mysqli-lite_php_browscap
+  # Docker Push Image
+  docker push djpic/php:$php_version-mysqli
+  docker push djpic/php:$php_version-mysqli-browscap
+  docker push djpic/php:$php_version-mysqli-full_php_browscap
+  docker push djpic/php:$php_version-mysqli-lite_php_browscap
+
+  cd ../../
+done
