f45a466a35635a5b3bc0007f0a4953c4f7552f8b
djpic <djpic@icloud.com>
2022-12-01 22:08:44 -0500

    Update build file with shorthand for docker-default and docker-dev.

--- Cron/build.sh
+++ Cron/build.sh
@@ -10,10 +10,12 @@ docker tag djpic/cron:$current_alpine_version-standard djpic/cron:latest
 # Build default cron image
 cd default
 docker build --build-arg alpine_version=$current_alpine_version --tag djpic/cron:$current_alpine_version-default .
+docker tag djpic/cron:$current_alpine_version-default djpic/cron:default
 
 # Build development cron image
 cd ../development/
 docker build --build-arg alpine_version=$current_alpine_version --tag djpic/cron:$current_alpine_version-dev .
+docker tag djpic/cron:$current_alpine_version-dev djpic/cron:dev
 
 # Push images to Dockerhub
 docker push djpic/cron:latest
