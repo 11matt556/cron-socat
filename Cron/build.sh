a9ec2ed0737ffaee4357c5c427449fedda217ad2
djpic <djpic@icloud.com>
2023-02-14 08:02:24 -0500

    Update alpine linux for cron image to 3.16.4 from 3.16.3

--- Cron/build.sh
+++ Cron/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # What base image of Alpine to use for all builds
-current_alpine_version=3.16.3
+current_alpine_version=3.16.4
 
 # Build standard cron image
 docker build --build-arg alpine_version=$current_alpine_version --tag djpic/cron:$current_alpine_version-standard .
