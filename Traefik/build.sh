13d0432b34644e6063cff72a31523a990558152c
djpic <djpic@icloud.com>
2023-04-21 21:27:01 -0400

    Update Traefik version from 2.9.9 to 2.9.10

--- Traefik/build.sh
+++ Traefik/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # What base image of Traefik to use for all builds
-current_traefik_version=2.9.9
+current_traefik_version=2.9.10
 
 # Build Traefik with default dynamic configuration
 cd tls
