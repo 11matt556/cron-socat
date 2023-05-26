f472e33fb239cffa3225b501f2e475208a060f8e
djpic <djpic@icloud.com>
2023-05-25 20:01:04 -0400

    Update php versions: 8.1.18 to 8.1.19 8.2.5 to 8.2.6

--- PHP/build.sh
+++ PHP/build.sh
@@ -2,7 +2,7 @@
 
 # Declare array to define the two different versions that will be used
 # First one is the current stable version; the first is the newer supported version
-php_versions=(8.0.28 8.1.18 8.2.5)
+php_versions=(8.0.28 8.1.19 8.2.6)
 
 for php_version in "${php_versions[@]}"
 do
