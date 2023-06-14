74193cc65e88c340e10b39705fdcf9202f584a0f
Matthew Howell <llmatt556@hotmail.com>
2023-06-14 19:12:25 -0400

    Remove non-cron files

--- Cron/development/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Running default command...." > /dev/stdout
-SCRIPT_NAME=/app/private/scheduled_jobs/development/1min.php \
-SCRIPT_FILENAME=/app/private/scheduled_jobs/development/1min.php \
-REQUEST_METHOD=GET \
-cgi-fcgi -bind -connect php:9000
