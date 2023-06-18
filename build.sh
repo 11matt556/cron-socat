1cf5d24b9a48418c381ca3bceb10c53bb4ecea0b
Matthew Howell <llmatt556@hotmail.com>
2023-06-17 21:22:38 -0400

    Gave up on following Github docs and doing it my way instead

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 # What base image of Alpine to use for all builds
-current_alpine_version=1.7.4.4
+current_alpine_version=$1
+
+if [ $# -eq 0 ];
+then
+	current_alpine_version=latest
+fi
 
 # Build standard cron image
 docker build --build-arg alpine_socat_version=$current_alpine_version --tag docker.io/11matt556/socat-cron:$current_alpine_version .
 docker build --build-arg alpine_socat_version=$current_alpine_version --tag ghcr.io/11matt556/socat-cron:$current_alpine_version .
-
-# Push images to Dockerhub
-docker push ghcr.io/11matt556/socat-cron:$current_alpine_version
-#docker push docker.io/11matt556/socat-cron:latest
-#docker push docker.io/11matt556/socat-cron:$current_alpine_version
