a00e354a63729ff3fb8ee24b2410798cad2d6ebd
Matthew <mrhowel@g.clemson.edu>
2023-06-18 21:44:37 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "Based on https://hub.docker.com/repository/docker/djpic/cron"
-echo "Starting DockerContainer..."
+echo "For more information: https://github.com/11matt556/socat-cron"
+echo "Starting Docker Container..."
 
 crond -f -l 8 -d 8 -L /dev/stdout
